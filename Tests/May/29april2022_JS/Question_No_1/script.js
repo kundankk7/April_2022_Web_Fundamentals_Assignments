ec3f6df2d35e485474831cd7017a5e0e301bfc97
Kundan Kumar <kundnkumar183@gmail.com>
2022-05-29 22:23:27 +0530

    Add files via upload

---
+++ Tests/May/29april2022_JS/Question_No_1/script.js
@@ -0,0 +1,19 @@
+// This is Eg. of Var
+console.log("This is Eg. of Var");
+var a=100;
+var b=100;
+console.log(a+b);
+
+
+// This is Eg. of let
+let c=300;
+let d=200;
+console.log("This is Eg. of let");
+console.log(c-d);
+
+
+// This is Eg. of const
+const e=100;
+const f=2000;
+console.log("This is Eg. of const");
+console.log(e*f);
