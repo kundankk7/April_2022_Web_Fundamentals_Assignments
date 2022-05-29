758b98c33fff21424ef04e2f93a22fd93a2ec229
Kundan Kumar <kundnkumar183@gmail.com>
2022-05-29 23:42:20 +0530

    Update script.js

--- Tests/May/29april2022_JS/Question_No_2/script.js
+++ Tests/May/29april2022_JS/Question_No_2/script.js
@@ -1,17 +1,1 @@
-// Null
-let name = null;
-console.log("Example of Null");
-document.write("*Example of Null also see in console  <br> <br>");
-console.log(name); //null
 
-// Undefinde
-let name1;
-console.log("Example of undefined");
-console.log(name1); //undefined
-document.write("*Example of undefined also see in console  <br>  <br>");
-
-
-document.write("*Example of not defined also see in console  <br>  <br>");
-
-console.log("Example of Not defined");
-console.log(b);
